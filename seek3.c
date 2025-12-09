7542a5cffb090e9f5ad4869de925212e348f1a72
Anne Karoline Goulart <annekgoulart@gmail.com>
2025-12-09 00:58:42 -0300

    Create seek3.c

---
+++ seek3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main (){
+    FILE* arq= fopen("arq.txt", "r+");
+    if(!arq){
+        printf("Erro ao abrir o arquivo!");
+        return 1;
+    }
+    fseek(arq, 0, seek_end);
+    tamanho= ftell(arq);
+    
+    meio= tamanho/2;
+    
+    fseek(arq, meio, seek_set);
+    resto = malloc(tamanho - meio + 1);
+    fread(resto, sizeof(char), tamanho-meio, arq);
+    
+    fseek(arq, meio, seek_set);
+    fwrite("|meio|", sizeof(char), 6, arq);
+    fwrite(resto, sizeof(char), tamanho-meio, arq);
+    
+    free(resto);
+    fclose(arq);
+    
+    return 0;
+}
